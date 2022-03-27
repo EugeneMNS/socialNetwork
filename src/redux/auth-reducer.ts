e9c84d03fa81dc44348677a0e20673e5e8d308ee
EugeneMNS <evgen.str4@gmail.com>
2022-03-27 19:23:58 +0300

    68 redirect

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,3 +1,7 @@
+import {ThunkAction} from "redux-thunk";
+import {AppStateType} from "./store";
+import {authApi} from "../api/api";
+
 const SET_AUTH_USER_DATA = 'SET-AUTH-USER-DATA'
 
 
@@ -30,8 +34,19 @@ export const authReducer = (state: AuthDataType = initialState, action: ActionTy
     }
 }
 
-export const setAuthUserData = (userId: number | null, email: string | null, login: string | null) => ({
+const setAuthUserData = (userId: number | null, email: string | null, login: string | null) => ({
     type: SET_AUTH_USER_DATA,
     data: {userId, email, login}
 } as const)
 type SetAuthUserDataType = ReturnType<typeof setAuthUserData>
+
+type ThunkType=ThunkAction<void, AppStateType, undefined, ActionType>
+export const getAuthUserData = (): ThunkType =>(dispatch) =>{
+    authApi.me()
+        .then((data: any)=>{
+            if (data.resultCode === 0){
+                const {id, email, login}=data.data
+                dispatch(setAuthUserData(id,email, login))
+            }
+        })
+}
